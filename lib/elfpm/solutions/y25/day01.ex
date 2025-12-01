e9a1691ff1fc32244ed95904d5e5286c0a119ff9
Avi Agarwal <aviagarwal1212@gmail.com>
2025-12-01 13:34:22 +0530

    D01P1 initial attempt complete

--- lib/elfpm/solutions/y25/day01.ex
+++ lib/elfpm/solutions/y25/day01.ex
@@ -18,17 +18,43 @@ defmodule Elfpm.Solutions.Y25.Day01 do
     # |> Input.stream!()
     # |> Enum.map!(&my_parse_line_function/1)
 
-    Input.read!(input)
+    # Input.read!(input)
+
+    input
+    |> Input.read!()
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&line_parser/1)
   end
 
   def part_one(problem) do
     # This function receives the problem returned by parse/2 and must return
     # today's problem solution for part one.
 
-    problem
+    {final_count, _final_position} =
+      problem
+      |> Enum.map(&movement_parser/1)
+      |> Enum.reduce({0, 50}, fn element, {count, position} ->
+        case value_cycler(position + element) do
+          0 -> {count + 1, 0}
+          new_position -> {count, new_position}
+        end
+      end)
+
+    final_count
   end
 
   # def part_two(problem) do
   #   problem
   # end
+  #
+
+  defp line_parser(<<direction::binary-size(1), distance::binary>>) do
+    %{direction: direction, distance: String.to_integer(distance)}
+  end
+
+  defp movement_parser(%{direction: "L", distance: distance}), do: -1 * distance
+  defp movement_parser(%{direction: "R", distance: distance}), do: distance
+
+  defp value_cycler(number), do: (rem(number, 100) + 100) |> rem(100)
 end
--- test/2025/day01_test.exs
+++ test/2025/day01_test.exs
@@ -31,6 +31,32 @@ defmodule Elfpm.Solutions.Y25.Day01Test do
 
   test "part one example" do
     input = ~S"""
+    L68
+    L30
+    R48
+    L5
+    R60
+    L55
+    L1
+    L99
+    R14
+    L82
+    """
+
+    assert 3 == solve(input, :part_one)
+  end
+
+  # Once your part one was successfully sumbitted, you may uncomment this test
+  # to ensure your implementation was not altered when you implement part two.
+
+  @part_one_solution 1026
+
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2025, 1, :part_one)
+  end
+
+  test "part two example" do
+    input = ~S"""
     This is an
     example input.
     replace with
@@ -38,29 +64,8 @@ defmodule Elfpm.Solutions.Y25.Day01Test do
     the AoC website.
     """
 
-    assert CHANGE_ME == solve(input, :part_one)
+    assert CHANGE_ME == solve(input, :part_two)
   end
-
-  # Once your part one was successfully sumbitted, you may uncomment this test
-  # to ensure your implementation was not altered when you implement part two.
-
-  # @part_one_solution CHANGE_ME
-  #
-  # test "part one solution" do
-  #   assert {:ok, @part_one_solution} == AoC.run(2025, 1, :part_one)
-  # end
-
-  # test "part two example" do
-  #   input = ~S"""
-  #   This is an
-  #   example input.
-  #   replace with
-  #   an example from
-  #   the AoC website.
-  #   """
-  #
-  #   assert CHANGE_ME == solve(input, :part_two)
-  # end
 
   # You may also implement a test to validate the part two to ensure that you
   # did not broke your shared modules when implementing another problem.
