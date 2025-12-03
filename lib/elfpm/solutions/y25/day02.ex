da28aa52580915bb658e29fbe50d0b9f1376eaa5
Avi Agarwal <aviagarwal1212@gmail.com>
2025-12-03 16:27:53 +0530

    refactor D02P1

--- lib/elfpm/solutions/y25/day02.ex
+++ lib/elfpm/solutions/y25/day02.ex
@@ -14,56 +14,29 @@ defmodule Elfpm.Solutions.Y25.Day02 do
 
   def part_one(problem) do
     problem
-    |> Enum.flat_map(&process_range/1)
+    |> Enum.flat_map(fn [head, tail] -> head..tail end)
+    |> Enum.filter(&check_repeating_chunks(&1, 2))
     |> Enum.sum()
   end
 
-  defp process_range([head, tail]) do
-    head..tail
-    |> Enum.filter(&check_numbers_one/1)
-  end
-
-  defp check_numbers_one(input) do
-    string = Integer.to_string(input)
-    len = String.length(string)
+  def check_repeating_chunks(number, chunk_count) do
+    digits = Integer.digits(number)
 
     cond do
-      rem(len, 2) == 0 ->
-        size = div(len, 2)
-        string |> chunk_string(size) |> check_repeating_string()
+      rem(length(digits), chunk_count) == 0 ->
+        size = div(length(digits), chunk_count)
+        digits |> Enum.chunk_every(size) |> check_repeating_items()
 
       true ->
         false
     end
   end
 
-  def part_two(problem) do
-    problem
-    |> Enum.flat_map(&process_range_two/1)
-  end
+  def check_repeating_items([head | rest]), do: Enum.all?(rest, fn item -> item == head end)
+  def check_repeating_items(_list), do: false
 
-  def check_number(input, 0, state) do
-    state
-    |> Enum.any?()
-  end
-
-  defp check_number(input, size, state) do
-    state =
-      cond do
-        rem(String.length(input), size) == 0 ->
-          [input |> chunk_string(size) |> check_repeating_string() | state]
-
-        true ->
-          [false | state]
-      end
-
-    check_number(input, size - 1, state)
-  end
-
-  defp chunk_string(input, size) do
-    for <<chunk::size(size)-binary <- input>>, do: chunk
-  end
-
-  defp check_repeating_string([head | rest]), do: Enum.all?(rest, fn chunk -> chunk == head end)
-  defp check_repeating_string(_input), do: false
+  # def part_two(problem) do
+  #   problem
+  #   |> Enum.flat_map(&process_range_two/1)
+  # end
 end
